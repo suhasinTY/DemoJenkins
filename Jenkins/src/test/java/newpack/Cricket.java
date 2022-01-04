fe0ff84c013b1de7225c4ca1f2c2cf9066e7ace3
USER <USER@suhas>
2022-01-04 17:08:13 +0530

    pushed successfully

---
+++ Jenkins/src/test/java/newpack/Cricket.java
@@ -0,0 +1,13 @@
+package newpack;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Cricket {
+	
+	@Test
+	public void createAccount() {
+		Reporter.log("welcome",true);
+	}
+
+}
